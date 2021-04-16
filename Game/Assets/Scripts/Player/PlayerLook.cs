80cd531a9941b4f018fe5d9dd608339c8e913d01
Chris Husky <chrishuskywolf@gmail.com>
2021-04-16 12:19:59 +0200

    TestMovement

---
+++ Game/Assets/Scripts/Player/PlayerLook.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour {
+
+    public Transform Player;
+    public float MouseSensitivity = 10;
+    private float x = 0;
+    private float y = 0;
+
+    private void Start() {
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+    }
+
+    private void Update() {
+
+        x += -Input.GetAxis("Mouse Y") * MouseSensitivity;
+        y += Input.GetAxis("Mouse X") * MouseSensitivity;
+
+        x = Mathf.Clamp(x, -90, 90);
+
+        transform.localRotation = Quaternion.Euler(x, 0, 0);
+        Player.localRotation = Quaternion.Euler(0, y, 0);
+
+    }
+
+}
---
+++ Game/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float Mass;
+    public float MovementSpeed;
+    public float JumpForce;
+    public bool isGrounded;
+    public LayerMask layerMask;
+    private Rigidbody rb;
+
+    private void Start() {
+
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    private void Update() {
+
+        float x = Input.GetAxisRaw("Horizontal") * MovementSpeed;
+        float y = Input.GetAxisRaw("Vertical") * MovementSpeed;
+
+        isGrounded = Physics.CheckSphere(new Vector3(transform.position.x, transform.position.y - 1, transform.position.z), 0.4f, layerMask);
+
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded) {
+
+            rb.velocity = new Vector3(rb.velocity.x, JumpForce, rb.velocity.z);
+
+        }
+
+        if(!isGrounded) {
+
+            rb.velocity -= new Vector3(0, Mass, 0);
+
+        }
+
+        Vector3 movePos = transform.right * x + transform.forward * y;
+        Vector3 newMovePos = new Vector3(movePos.x, rb.velocity.y, movePos.z);
+
+        rb.velocity = newMovePos;
+
+    }
+
+}
