e920541af32d90e7dcd374b8197bf118e9a4b92d
Lobster727 <dukedavin2@gmail.com>
2021-04-16 13:30:49 -0400

    Mouse Movement
    Redid the mouse code

--- Game/Assets/Scripts/Player/MouseLook.cs
+++ Game/Assets/Scripts/Player/MouseLook.cs
@@ -2,24 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MouseLook : MonoBehaviour 
+public class MouseLook : MonoBehaviour
 {
+
     public float mouseSensitivity = 100f;
 
-    public transform playerBody;
+    public Transform playerBody;
 
-    private void Start() 
+    float xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
     {
-
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    private void Update() 
+    // Update is called once per frame
+    void Update()
     {
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-        playerBody.rotate(Vector3.up * mouseX);
-    }
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-}
-// To be continued
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+
+
+    }
+}//Lobster was here
