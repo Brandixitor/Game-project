bf634344fe6a545e1e225767309c3df399f52151
Brandixitor <med.salah.bourouba@gmail.com>
2021-04-16 14:13:04 +0100

    Script update

--- Game/Assets/Scripts/Player/MouseLook.cs
+++ Game/Assets/Scripts/Player/MouseLook.cs
@@ -2,30 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerLook : MonoBehaviour {
+public class MouseLook : MonoBehaviour 
+{
+    public float mouseSensitivity = 100f;
 
-    public Transform Player;
-    public float MouseSensitivity = 10;
-    private float x = 0;
-    private float y = 0;
-
-    private void Start() {
-
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+    private void Start() 
+    {
 
     }
 
-    private void Update() {
-
-        x += -Input.GetAxis("Mouse Y") * MouseSensitivity;
-        y += Input.GetAxis("Mouse X") * MouseSensitivity;
-
-        x = Mathf.Clamp(x, -90, 90);
-
-        transform.localRotation = Quaternion.Euler(x, 0, 0);
-        Player.localRotation = Quaternion.Euler(0, y, 0);
-
+    private void Update() 
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
     }
 
 }
