2ddb5fe7ce34ac275f6163328131c167289b2782
Brandixitor <med.salah.bourouba@gmail.com>
2021-04-17 15:34:30 +0100

    Crouching done

---
+++ Game/Assets/Scripts/Player/PlayerCrouch.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCrouch : MonoBehaviour
+{
+    CapsuleCollider playerCol;
+    float originalHeight;
+    public float reducedHeight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerCol = GetComponent<CapsuleCollider>();
+        originalHeight = playerCol.height;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Crouching script.
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        crouch();
+            else if(Input.GetKeyUp(KeyCode.LeftControl)) 
+            GoUp();
+    }
+
+
+// Script to reduce height.
+void crouch()
+{
+   playerCol.height = reducedHeight; 
+}
+
+// Script to reset height.
+void GoUp()
+{
+   playerCol.height = originalHeight; 
+}
+
+}
